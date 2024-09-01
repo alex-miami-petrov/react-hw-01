eb1816479fa9e3a40ad989ba0c0b0aff67602b2f
Miami <alex.miami.petrov@gmail.com>
2024-09-01 10:26:36 +0300

    m

--- src/components/FriendList/friendList.jsx
+++ src/components/FriendList/friendList.jsx
@@ -1,10 +1,32 @@
-import React from "react";
+// import React from "react";
 
+// import FriendListItem from "../FriendListItem/friendListItem";
+
+// const FriendList = ({ friends }) => {
+//   return (
+//     <ul className="friendList">
+//       {friends.map((friend) => (
+//         <li key={friend.id}>
+//           <FriendListItem
+//             avatar={friend.avatar}
+//             name={friend.name}
+//             isOnline={friend.isOnline}
+//           />
+//         </li>
+//       ))}
+//     </ul>
+//   );
+// };
+
+// export default FriendList;
+
+import React from "react";
+import styles from "./friendList.module.css";
 import FriendListItem from "../FriendListItem/friendListItem";
 
 const FriendList = ({ friends }) => {
   return (
-    <ul className="friendList">
+    <ul className={styles.friendList}>
       {friends.map((friend) => (
         <li key={friend.id}>
           <FriendListItem
--- src/components/FriendListItem/friendListItem.jsx
+++ src/components/FriendListItem/friendListItem.jsx
@@ -1,17 +1,43 @@
+// import React from "react";
+// import "./friendListItem.css";
+
+// const FriendListItem = ({ avatar, name, isOnline }) => {
+//   return (
+//     <div className="friend">
+//       <img
+//         src={avatar}
+//         alt={`Avatar of ${name}`}
+//         width="48"
+//         className="friend__avatar"
+//       />
+//       <p className="friend__name">{name}</p>
+//       <p className={`friend__status ${isOnline ? "online" : "offline"}`}>
+//         {isOnline ? "Online" : "Offline"}
+//       </p>
+//     </div>
+//   );
+// };
+
+// export default FriendListItem;
+
 import React from "react";
-import "./friendListItem.css";
+import styles from "./friendListItem.module.css";
 
 const FriendListItem = ({ avatar, name, isOnline }) => {
   return (
-    <div className="friend">
+    <div className={styles.friend}>
       <img
         src={avatar}
         alt={`Avatar of ${name}`}
         width="48"
-        className="friend__avatar"
+        className={styles.friend__avatar}
       />
-      <p className="friend__name">{name}</p>
-      <p className={`friend__status ${isOnline ? "online" : "offline"}`}>
+      <p className={styles.friend__name}>{name}</p>
+      <p
+        className={`${styles.friend__status} ${
+          isOnline ? styles.online : styles.offline
+        }`}
+      >
         {isOnline ? "Online" : "Offline"}
       </p>
     </div>
